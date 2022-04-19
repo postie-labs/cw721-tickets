a5eab6a4278934b671f18fc5dd7c465fe551f294
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-04-20 05:52:38 +0900

    Fix typo

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,9 +1,6 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Empty;
-use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
-
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
 pub struct Attribute {
     pub key: String,
@@ -12,9 +9,9 @@ pub struct Attribute {
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
 pub struct Metadata {
-    pub not_valid_before: i64;
-    pub not_valid_after: i64;
-    pub attributes: Option<Attribute>
+    pub not_valid_before: i64,
+    pub not_valid_after: i64,
+    pub attributes: Option<Attribute>,
 }
 
 pub type Extension = Option<Metadata>;
