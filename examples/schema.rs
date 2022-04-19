50de78d76de846e98b6fd7a1afb8b149e0a80b1b
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-04-20 06:54:02 +0900

    Update package name

--- examples/schema.rs
+++ examples/schema.rs
@@ -7,7 +7,7 @@ use cw721::{
     AllNftInfoResponse, ApprovalResponse, ApprovalsResponse, ContractInfoResponse, NftInfoResponse,
     NumTokensResponse, OperatorsResponse, OwnerOfResponse, TokensResponse,
 };
-use cw721_tickets_contract::{ExecuteMsg, Extension, InstantiateMsg, MinterResponse, QueryMsg};
+use cw721_tickets::{ExecuteMsg, Extension, InstantiateMsg, MinterResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
