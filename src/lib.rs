433e39eea5deb2a912788f4e06b5800060da67e8
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-04-20 06:19:53 +0900

    Re-define entrypoint

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+mod metadata;
+
+pub use crate::metadata::Extension;
+
+use cosmwasm_std::Empty;
+pub use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+
+pub type Cw721TicketsContract<'a> = cw721_base::Cw721Contract<'a, Extension, Empty>;
+pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension>;
+
+#[cfg(not(feature = "library"))]
+pub mod entry {
+    use super::*;
+
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+    #[entry_point]
+    pub fn instantiate(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> StdResult<Response> {
+        Cw721TicketsContract::default().instantiate(deps, env, info, msg)
+    }
+
+    #[entry_point]
+    pub fn execute(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        Cw721TicketsContract::default().execute(deps, env, info, msg)
+    }
+
+    #[entry_point]
+    pub fn query(
+        deps: Deps,
+        env: Env,
+        msg: QueryMsg,
+    ) -> StdResult<Binary> {
+        Cw721TicketsContract::default().query(deps, env, msg)
+    }
+}
